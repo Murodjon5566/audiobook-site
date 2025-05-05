dbf8c7502ad7668867316228caca226aadca03dc
Davron-Adbusalomov <2110117@newuzbekistanuniversity.uz>
2025-05-05 23:25:43 +0500

    bug fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // src/main.js
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './index.js' // bu routerni olib keladi
+import router from './router'
 
 createApp(App).use(router).mount('#app') // bu routerni ulaydi
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import WorldLiterature from '../views/WorldLiterature.vue'
-import NationalLiterature from '../views/NationalLiterature.vue'
+import WorldLiterature from '../pages/WorldLiterature.vue'
+import NationalLiterature from '../pages/NationalLiterature.vue'
 
 const routes = [
-  { path: '/world', component: WorldLiterature },
-  { path: '/national', component: NationalLiterature }
+    { path: '/world', component: WorldLiterature },
+    { path: '/national', component: NationalLiterature }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+    history: createWebHistory(),
+    routes
 })
 
 export default router
